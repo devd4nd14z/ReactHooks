8728ae780c3cfe687823ac11f30758acad810e56
d4nd14z <dev.dandiaz@gmail.com>
2022-12-22 09:16:30 -0500

    Creacion inicial del Proyecto - Zero Commit

---
+++ src/HooksApp.jsx
@@ -0,0 +1,9 @@
+export const HooksApp = () => {
+    return (
+        <div className="container px-2 py-2">
+            <div className="row">
+                <div className="col-12"><h1 className="text-3xl font-extrabold text-indigo-700">Hooks App</h1></div>                
+            </div>            
+        </div>
+    );
+}
